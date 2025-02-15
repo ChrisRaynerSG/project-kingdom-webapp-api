f4f5af33740c3c786daef66c88f35880a8a2d502
Chris <carnasagames@gmail.com>
2025-02-15 17:08:58 +0000

    add Test classes for service layer

---
+++ src/test/java/com/carnasa/cr/projectkingdomwebpage/services/impl/ForumServiceImplTest.java
@@ -0,0 +1,17 @@
+package com.carnasa.cr.projectkingdomwebpage.services.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ForumServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
---
+++ src/test/java/com/carnasa/cr/projectkingdomwebpage/services/impl/UserServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.carnasa.cr.projectkingdomwebpage.services.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUserById() {
+    }
+
+    @Test
+    void getUserDtoById() {
+    }
+
+    @Test
+    void getAllUsers() {
+    }
+
+    @Test
+    void saveUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteUser() {
+    }
+}
